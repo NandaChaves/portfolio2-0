e2f67ed0905e01a977df31e54de1f939b1697be6
FernandaFortes <fernandafortesch@gmail.com>
2026-03-13 13:06:35 +0000

    fix: layout adjustment height

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -14,7 +14,7 @@ const Section = (props) => {
   const { children } = props;
 
   return (
-    <motion.section className={`min-h-screen w-full p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}
+    <motion.section className={`h-screen w-full p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center`}
       initial={{
         opacity: 0,
         y: 50,
